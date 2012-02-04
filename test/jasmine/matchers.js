0bd322a3fa4e8915c758f6871a2bb647dda789da
David Kemp <davidgithub@lizanddavid.com>
2012-02-04 17:44:33 +1100

    removing tests from documentation pages.

--- test/jasmine/matchers.js
+++
@@ -1,6 +0,0 @@
-jsqbits.JasmineMatchers = jsqbits.JasmineMatchers || {};
-
-jsqbits.JasmineMatchers.toBeApprox = function(expected) {
-    return  Math.abs(this.actual.real - expected.real) < 0.0001 &&
-                Math.abs(this.actual.imaginary - expected.imaginary) < 0.0001
-}

9faf818d2be940d78dc24c53c1752ca7387e29f4
David Kemp <davidgithub@lizanddavid.com>
2012-02-15 22:50:49 +1100

    Write more of the user manual

--- doc/jsqbitsManual.js
+++ doc/jsqbitsManual.js
@@ -20,5 +20,19 @@ new function($) {
         if (totalErrors > 0) {
             $('#topErrorMessage').show();
         }
+
+        var indexItems = [];
+        var indexMap = [];
+        $('ul.index li, h2.index').map(function() {
+            var indexItem = $(this).text().split('(')[0];
+            indexItems.push(indexItem);
+            indexMap[indexItem] = $(this).closest('section').find('h2').attr('id');
+        });
+        indexItems.sort();
+        for (var i in indexItems) {
+            var indexItem = indexItems[i];
+            var element = $('<a>').attr('href', '#' + indexMap[indexItem]).text(indexItem + ' ');
+            $('#tableOfContents').append(element);
+        }
     });
-}(jQuery);
+}(jQuery);  

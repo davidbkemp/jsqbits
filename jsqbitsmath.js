8ce90229b20b26f1fdc9d31dc24e83e00a627fff
David Kemp <davidgithub@lizanddavid.com>
2012-03-13 21:27:56 +1100

    start working on some utilities to help with Simon's algorithm

---
+++ jsqbitsmath.js
@@ -0,0 +1,92 @@
+/**
+ *    Copyright 2012 David Kemp
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+/**
+ * For documentation, and the latest version, see http://www.jsqbits.org/
+ */
+
+var jsqbitsmath = jsqbitsmath || {};
+
+(function() {
+
+    /**
+     * Find the null space in modulus 2 arithmetic of a matrix of binary values
+     * @param a matrix of binary values represented using an array of numbers
+     * whose bit values are the entries of a matrix rowIndex.
+     * @param width the width of the matrix.
+     */
+    jsqbitsmath.findNullSpaceMod2 = (function() {
+
+        var attemptToMakePivot = function(a, colIndex, pivotRowIndex) {
+            var colBitMask = 1 << colIndex;
+            if (colBitMask & a[pivotRowIndex]) return;
+            for (var rowIndex = pivotRowIndex + 1; rowIndex < a.length; rowIndex ++) {
+                if (colBitMask & a[rowIndex]) {
+                    var tmp = a[pivotRowIndex];
+                    a[pivotRowIndex] = a[rowIndex];
+                    a[rowIndex] = tmp;
+                }
+            }
+        };
+
+        var makeTriangularFormMod2 = function(a, width) {
+            var pivotRowIndex = 0;
+            for (var pivotColIndex = width - 1; pivotColIndex >= 0; pivotColIndex--) {
+                attemptToMakePivot(a, pivotColIndex, pivotRowIndex);
+                var colBitMask = 1 << pivotColIndex;
+                if (colBitMask & a[pivotRowIndex]) {
+                    zeroOutBelow(a, pivotColIndex, pivotRowIndex);
+                    pivotRowIndex++;
+                }
+            }
+        };
+
+        var zeroOutBelow = function(a, pivotColIndex, pivotRowIndex) {
+            var pivotRow = a[pivotRowIndex];
+            var colBitMask = 1 << pivotColIndex;
+            for (var rowIndex = pivotRowIndex + 1; rowIndex < a.length; rowIndex++) {
+                if (colBitMask & a[rowIndex]) {
+                    a[rowIndex] = a[rowIndex] ^ pivotRow;
+                }
+            }
+        };
+
+        var specialSolutions = function(a, width) {
+            specialSolutions = [];
+            var rowIndex = 0;
+            for (var colIndex  = width - 1; colIndex >= 0; colIndex--) {
+                var colBitMask = 1 << colIndex;
+                if ()
+            }
+            return specialSolutions;
+        };
+
+        return function(a, width) {
+            a = cloneArray(a);
+            makeTriangularFormMod2(a, width);
+            return specialSolutions(a, width);
+        };
+    })();
+
+    var cloneArray = function(a) {
+        var result = [];
+        for (var i = 0; i < a.length; i++) {
+            result[i] = a[i];
+        }
+        return result;
+    };
+
+})();
---
+++ test/jasmine/spec/jsqbitsmath_spec.js
@@ -0,0 +1,16 @@
+describe("jsqbitsmath", function() {
+
+    describe("#nullSpace", function() {
+        it("should solve Ax=0", function() {
+            var a = [
+                parseInt('001', 2),
+                parseInt('111', 2),
+                parseInt('110', 2),
+                parseInt('000', 2)
+            ];
+            var results = jsqbitsmath.findNullSpaceMod2(a, 3);
+            expect(results).toBe([parseInt('110', 2)]);
+        });
+    });
+
+});

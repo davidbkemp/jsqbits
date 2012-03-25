65793ac471a14cd702b41dc94fe2ae4fc98858e1
David Kemp <davidbkemp@gmail.com>
2012-03-25 21:13:20 +1100

    Added a tar step to package.sh

--- utils/package.sh
+++ utils/package.sh
@@ -8,13 +8,16 @@ die () {
 [ "$#" -eq 1 ] || die "1 argument required, $# provided"
 echo creating release $1
 
+mkdir -p jsqbits-doc-$1
+rm -r jsqbits-doc-$1/*
+rm jsqbits-doc-$1.tar.gz
+
 git checkout $1
 
 [ $? -eq 0 ] || die "Could not checkout the git tag"
 
-mkdir -p jsqbits-doc-$1
-rm -r jsqbits-doc-$1/*
-
 cp -r jsqbits-$1.js jsqbitsManual.html jsqbitsTutorial.html jsqbitsRunner.html LICENSE README css js jsqbits-doc-$1
+
+tar -czf jsqbits-doc-$1.tar.gz jsqbits-doc-$1
 
 git checkout master

905e156d5a5bf452e2b29784a56144e0655d4106
David Kemp <davidbkemp@gmail.com>
2012-09-27 19:57:44 +1000

    Modify jsqbitsRunner to support 'printing' to a 'console' and modify the examples to make use of this.  Make the general period finding example use setTimeout to occassionally give control back to the browser

---
+++ resources-1.7/js/jsqbitsRunner.js
@@ -0,0 +1,60 @@
+var ALL = jsqbits.ALL;
+
+$(function() {
+
+    $('#run').click(function() {
+        clearConsole();
+        var result;
+        try {
+            result = eval($('#code').get(0).value);
+            if ((typeof result === 'object') && result.toString) {
+                result = result.toString();
+            } else if (typeof result === 'number') {
+                result = '' + result;
+            }
+        } catch (e) {
+            result = e.message ? e.message : e;
+        }
+        if (result) {
+            $('#result').text(result);
+        }
+    });
+
+    function clearConsole() {
+        $('#console').html('');
+    }
+
+    function clear() {
+       $('#code').val('');
+       $('#result').text('');
+       clearConsole();
+    }
+
+    $('#clear').click(function() {
+        clear();
+        $('#example').attr('value', 'none');
+    });
+
+    $('#example').change(function(event) {
+        var selectedExample = $(this).attr('value');
+        if (selectedExample === 'none') return;
+
+        $.get("resources-1.7/examples/" + selectedExample + ".js.example", function(data) {
+            clear();
+            $('#code').val(data);
+          })
+          .error(function() { alert("Sorry. Something went wrong."); });
+    });
+});
+
+
+function print(str) {
+    $("#console").append($("<div>").text(str));
+}
+
+function promptForFunction(message, example) {
+    var input = prompt(message, example);
+    var f;
+    eval("f = " + input);
+    return f;
+}
